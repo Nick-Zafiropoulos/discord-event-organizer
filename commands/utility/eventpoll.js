8e80bbba2a4207960df22b9cef9240107bfb05ad
Nick Zafiropoulos <nick.zaff@gmail.com>
2023-08-15 13:39:45 -0700

    Begin poll embed structuring

--- commands/utility/eventpoll.js
+++ commands/utility/eventpoll.js
@@ -8,6 +8,7 @@ const {
     StringSelectMenuBuilder,
     StringSelectMenuOptionBuilder,
     ComponentType,
+    EmbedBuilder,
 } = require('discord.js');
 
 module.exports = {
@@ -24,10 +25,16 @@ module.exports = {
             label: 'Enter the options for your poll',
             style: TextInputStyle.Paragraph,
         });
+        const eventOrganizerReqResponses = new TextInputBuilder({
+            customId: 'eventOrganizerReqResponses',
+            label: 'How many responses are required?',
+            style: TextInputStyle.Short,
+        });
 
         const inputFieldRow = new ActionRowBuilder().addComponents(eventOrganizerInput);
+        const inputFieldRowResponses = new ActionRowBuilder().addComponents(eventOrganizerReqResponses);
 
-        modal.addComponents(inputFieldRow);
+        modal.addComponents(inputFieldRow, inputFieldRowResponses);
 
         // Choose required response count
         const pollChoiceCount = new StringSelectMenuBuilder()
@@ -51,22 +58,74 @@ module.exports = {
         await interaction.showModal(modal);
 
         const filter = (interaction) => interaction.customId === `eventModal-${interaction.user.id}`;
-        interaction.awaitModalSubmit({ filter, time: 90_000 }).then((modalInteraction) => {
-            const eventOrganizerInputValue = modalInteraction.fields.getTextInputValue('eventOrganizerInput');
-            const optionsArr = eventOrganizerInputValue.split('\n');
-            console.log(optionsArr);
 
-            modalInteraction.reply({
-                content: `${optionsArr}\n\n How many responses are required?`,
-                components: [choiceDropdown],
+        if (interaction.awaitModalSubmit) {
+            interaction.awaitModalSubmit({ filter, time: 90_000 }).then((modalInteraction) => {
+                const eventOrganizerInputValue = modalInteraction.fields.getTextInputValue('eventOrganizerInput');
+                const eventOrganizerReqResponsesValue =
+                    modalInteraction.fields.getTextInputValue('eventOrganizerReqResponses');
+                const optionsArr = eventOrganizerInputValue.split('\n');
+                console.log(optionsArr);
+                console.log(eventOrganizerReqResponsesValue);
+
+                const reactionNums = [':one:', ':two:', ':three:', ':four:', ':five:'];
+
+                const embedOptionsArr = reactionNums.map(function (e, i) {
+                    return [e, optionsArr[i]];
+                });
+
+                // Set up poll options by checking length of options array, then add the numbers to it with a for loop
+                console.log(embedOptionsArr[0][1]);
+                const displayOptions = embedOptionsArr.join('\n').replaceAll(',', ' ');
+
+                const embed = new EmbedBuilder()
+                    .setTitle('Event Organizer Poll')
+                    // .setDescription('')
+                    .setColor('Random')
+                    .addFields({
+                        name: 'Respond to this event poll by selecting all options that work for you:',
+                        value: `${displayOptions}`,
+                        inline: true,
+                    });
+
+                modalInteraction.reply({
+                    embeds: [embed],
+                });
+
+                // modalInteraction.reply({
+                //     content: `${optionsArr}\n\n How many responses are required?`,
+                //     components: [choiceDropdown],
+                // });
+
+                // MANIPULATE THE DATA INTO PRESENTABLE EMBED WITH VOTING
             });
+        }
+        //  else if (interaction) {
+        //     const response = await interaction.reply({
+        //         //Content of message that actually gets sent
+        //         content: 'How many options do you want for your poll?',
+        //         components: [choiceDropdown],
+        //     });
 
-            // MANIPULATE THE DATA INTO PRESENTABLE EMBED WITH VOTING
-        });
+        //     //MULTIPLE RESPONSE FIELDS
+        //     const collector = response.createMessageComponentCollector({
+        //         componentType: ComponentType.StringSelect,
+        //         time: 3_600_000,
+        //     });
 
-        const response = await interaction.reply({
-            //Content of message that actually gets sent
-            content: 'How many options do you want for your poll?',
-        });
+        //     const selectionsArr = [];
+
+        //     collector.on('collect', async (i) => {
+        //         const selection = i.values[0];
+        //         selectionsArr.push(selection);
+
+        //         // await i.update(`${i.user} has selected ${selection}!`);
+
+        //         await i.update({
+        //             content: `${selectionsArr}`,
+        //             components: [choiceDropdown],
+        //         });
+        //     });
+        // }
     },
 };
